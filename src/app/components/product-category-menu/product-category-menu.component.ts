6d523ab8ef7cf9e717190484113c19abd9ecd9da
Dayakarms07 <dayakarr177@gmail.com>
2026-02-10 20:05:16 +0530

    Shopping Cart Development Completed

--- src/app/components/product-category-menu/product-category-menu.component.ts
+++ src/app/components/product-category-menu/product-category-menu.component.ts
@@ -19,7 +19,7 @@ export class ProductCategoryMenuComponent implements OnInit {
 
   listProductCategories() {
     this.productService.getProductCategories().subscribe((data) => {
-      console.log('product categories= ' + JSON.stringify(data));
+      // console.log('product categories= ' + JSON.stringify(data));
       this.productCategories = data;
     });
   }

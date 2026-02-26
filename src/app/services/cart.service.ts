3b00eae9d4d85804d2255dbab7195f5358ee7ca3
Dayakarms07 <dayakarr177@gmail.com>
2026-02-26 18:45:10 +0530

    Security Login Logout VIP member access handling browser refresh completed

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -11,7 +11,19 @@ export class CartService {
   totalPrice: Subject<number> = new BehaviorSubject<number>(0);
   totalQuantity: Subject<number> = new BehaviorSubject<number>(0);
 
-  constructor() {}
+  // storage: Storage = sessionStorage;
+    storage: Storage = localStorage; // data is persisted and survives browser restarts
+
+
+  constructor() {
+    // read data from storage
+    let data = JSON.parse(this.storage.getItem('cartItems')!);
+    if(data != null){
+      this.cartItems = data;
+    }
+    // compute totals based on the data that is read from storage
+    this.computeCartTotals();
+  }
 
   addToCart(theCartItem: CartItem) {
     let alreadyExistsInCart: boolean = false;
@@ -56,6 +68,9 @@ export class CartService {
     this.totalQuantity.next(totalQuantityValue);
 
     this.logCartData(totalPriceValue, totalQuantityValue);
+
+    //persist the cart data
+    this.persistCartItems();
   }
 
   logCartData(totalPriceValue: number, totalQuantityValue: number) {
@@ -86,5 +101,9 @@ export class CartService {
       this.cartItems.splice(itemIndex,1);
     }
     this.computeCartTotals();
+  }
+
+  persistCartItems(){
+    this.storage.setItem('cartItems', JSON.stringify(this.cartItems));
   }
 }
